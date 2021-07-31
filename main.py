6ae219fa708e9d4c8c539e22ef6dbcfee2524106
koyamal <tanakamaruko1121n@gmail.com>
2021-07-31 21:16:14 +0900

    Create clickablemap

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.secret_key = 'adfba2efkajd2fjlir37aerwe'
+
+@app.route('/')
+def top():
+    return render_template('main.html')
+
+@app.route('/prefecture/<prefName>')
+def hello_pref(prefName):
+    return render_template('hello_pref.html', prefName=prefName)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
